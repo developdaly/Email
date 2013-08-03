81153de899399b75a5af06ad29e6b7f0d1cfb7fa
Patrick Daly <patrick@developdaly.com>
2013-08-03 18:25:36 -0500

    Get the right modified author and display dates correctly in template

--- templates/new.php
+++ templates/new.php
@@ -5,7 +5,7 @@ function email_template( $action ) {
 	if( $action == 'new' ) {
 
 $output = 'A new [post_type] (#[post_id] "[post_title]") was created by [post_author]
-This action was taken on [post_date]
+This action was taken on [post_modified_date] at [post_modified_time]
 
 [old_status] => [new_status]
 
@@ -16,7 +16,7 @@ Title: [post_title]
 Author: [post_author] ([post_author_email])
 
 == Actions ==
-Edit: [edit_post]
+Edit: [edit_post_url]
 View: [permalink]
 
 --------------------
@@ -28,8 +28,8 @@ View: [permalink]
 
 	if( $action == 'updated' ) {
 
-$output = 'A [post_type] (#[post_id] "[post_title]") was updated by [post_author_modified]
-This action was taken on [post_modified_date] at [post_modfified_time]
+$output = 'A [post_type] (#[post_id] "[post_title]") was updated by [post_modified_author]
+This action was taken on [post_modified_date] at [post_modified_time]
 
 [old_status] => [new_status]
 
@@ -40,7 +40,7 @@ Title: [post_title]
 Author: [post_author] ([post_author_email])
 
 == Actions ==
-Edit: [edit_post]
+Edit: [edit_post_url]
 View: [permalink]
 
 --------------------
